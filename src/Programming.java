3f32eac470efd7430501e60a49e6ef083b6589cc
miriam199 <miriamaraia3@gmail.com>
2020-11-11 00:27:51 -0500

    initial commit

---
+++ src/Programming.java
@@ -0,0 +1,5 @@
+public class Programming {
+    public static void main(String[] args) {
+        System.out.println("Movie can be fun,at the same time boring ");
+    }
+}
--- src/hello.java
+++ src/hello.java
@@ -1,5 +1,7 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("java is not fan");
+
+        System.out.println("today is thursday ");
     }
 }
